543c9bebcde9a0ec0624dd006e98065ede843804
Greg Bergé <neoziro@gmail.com>
2013-10-26 02:11:31 +0200

    Database + crawler.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,19 @@
-var express = require('express');
+var express = require('express'),
+  Media = require('../models/media');
 
 var app = module.exports = express();
 
 app.use(express.json());
 app.get('/photos', photos);
 
-function photos(req, res) {
-  res.send({});
+function photos(req, res, next) {
+  Media
+    .find()
+    .sort('-created_time')
+    .skip(req.query.offset || 0)
+    .limit(req.query.limit || 20)
+    .exec(function (err, medias) {
+      if (err) return next(err);
+      res.send(medias);
+    });
 }
