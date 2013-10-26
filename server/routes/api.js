0741654059d0640e391fb17b87cabe81c5f19d97
Greg Bergé <neoziro@gmail.com>
2013-10-27 01:02:27 +0200

    Fix ranking.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+  async = require('async'),
   Media = require('../models/media');
 
 var app = module.exports = express();
@@ -14,8 +15,10 @@ app.get('/medias/:id/rank', rank);
 function list(req, res, next) {
   Media
     .find()
-    .sort('-likes.count')
-    .sort('-id')
+    .sort({
+      'likes.count': -1,
+      'id': -1
+    })
     .skip(req.query.skip || 0)
     .limit(req.query.limit || 20)
     .exec(function (err, medias) {
@@ -41,61 +44,82 @@ function get(req, res, next) {
 }
 
 function prevMedia(req, res, next) {
-  Media
-    .findOne({ id: req.params.id }, function (err, media) {
-      if (err) return next(err);
-      if (! media) return next(404);
+  async.waterfall([
+    computeRank.bind(null, req.params.id),
+    function (rank, cb) {
+      if (! rank) return cb();
 
       Media
-        .findOne({
-          'likes.count': { $gte: media.likes.count },
-          'id': { $ne: media.id }
+        .findOne()
+        .sort({
+          'likes.count': -1,
+          'id': -1
         })
-        .sort('likes.count')
-        .sort('id')
-        .exec(function (err, media) {
-          if (err) return next(err);
-          if (! media) return next(404);
+        .skip(rank - 1)
+        .limit(1)
+        .exec(cb);
+    }
+  ], function (err, media) {
+    if (err) return next(err);
+    if (! media) return next(404);
 
-          res.send(media);
-        });
-    });
+    res.send(media);
+  });
 }
 
 function nextMedia(req, res, next) {
-  Media
-    .findOne({ id: req.params.id }, function (err, media) {
-      if (err) return next(err);
-      if (! media) return next(404);
-
+  async.waterfall([
+    computeRank.bind(null, req.params.id),
+    function (rank, cb) {
       Media
-        .findOne({
-          'likes.count': { $lte: media.likes.count },
-          'id': { $ne: media.id }
+        .findOne()
+        .sort({
+          'likes.count': -1,
+          'id': -1
         })
-        .sort('-likes.count')
-        .sort('-id')
-        .exec(function (err, media) {
-          if (err) return next(err);
-          if (! media) return next(404);
+        .skip(rank + 1)
+        .limit(1)
+        .exec(cb);
+    }
+  ], function (err, media) {
+    if (err) return next(err);
+    if (! media) return next(404);
 
-          res.send(media);
-        });
-    });
+    res.send(media);
+  });
 }
 
 function rank(req, res, next) {
-  Media
-    .findOne({ id: req.params.id }, function (err, media) {
-      if (err) return next(err);
-      if (! media) return next(404);
+  computeRank(req.params.id, function (err, rank) {
+    if (err) return next(err);
+    res.send({ rank: rank });
+  });
+}
 
+function computeRank(id, callback) {
+  var _media, _count;
+
+  async.waterfall([
+    function (cb) {
+      Media.findOne({ id: id }, cb);
+    },
+    function (media, cb) {
+      _media = media;
       Media
         .count({
-          'likes.count': { $gt: media.likes.count }
-        }, function (err, count) {
-          if (err) return next(err);
-          res.send({ rank: count });
-        });
-    });
+          'likes.count': { $gt: _media.likes.count }
+        }, cb);
+    },
+    function (count, cb) {
+      _count = count;
+      Media
+        .count({
+          'likes.count': _media.likes.count,
+          'id': { $gt: _media.id }
+        }, cb);
+    }
+  ], function (err, count) {
+    if (err) return callback(err);
+    callback(null, _count + count);
+  });
 }
