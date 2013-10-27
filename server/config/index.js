31cedfa20ef419f2871ff888379ec452e9090733
Greg Bergé <neoziro@gmail.com>
2013-10-27 11:20:37 +0100

    Remove config from repo.

--- server/config/index.js
+++
@@ -1,5 +0,0 @@
-var nconf = require('nconf');
-
-module.exports = nconf
-  .file('env', __dirname + '/../../config/' + process.env.NODE_ENV + '.json')
-  .file(__dirname + '/../../config/default.json');
