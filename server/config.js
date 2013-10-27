6ab5e1dd12d52df43f66e620c95e8d2c811e8844
Greg Bergé <neoziro@gmail.com>
2013-10-27 18:54:25 +0100

    Rename config to envfile.

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ var nconf = require('nconf');
 
 module.exports = nconf
   .env('__')
-  .file('env', __dirname + '/../config/' + (process.env.NODE_ENV || 'development') + '.json')
+  .file('envfile', __dirname + '/../config/' + (process.env.NODE_ENV || 'development') + '.json')
   .file(__dirname + '/../config/default.json');
